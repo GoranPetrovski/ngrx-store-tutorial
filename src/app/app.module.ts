4c39ab7910934b07b86e718a4c8a08f6c02c6ae0
goran.petrovski <goki.petrovski@gmail.com>
2019-04-12 13:31:41 +0200

    craete blockchain model, component and reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { StoreModule } from '@ngrx/store';
 import { reducer } from './reducers/tutorial.reducer';
 import { ReadComponent } from './read/read.component';
 import { CreateComponent } from './create/create.component';
+import { BlockchainComponent } from './blockchain/blockchain.component';
+import { addCoinReducer } from './reducers/blockchain.reducer';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReadComponent,
-    CreateComponent
+    CreateComponent,
+    BlockchainComponent
   ],
   imports: [
     BrowserModule,
     StoreModule.forRoot({
-      tutorial: reducer
+      tutorial: reducer,
+      blockchain: addCoinReducer
     })
   ],
   providers: [],
--- src/app/app.state.ts
+++ src/app/app.state.ts
@@ -1,5 +1,7 @@
 import { Tutorial } from './models/tutorial.model';
+import { Blockchain } from './models/blockchain.model';
 
 export interface AppState {
   readonly tutorial: Tutorial[];
+  readonly blockchain: Blockchain[];
 }
